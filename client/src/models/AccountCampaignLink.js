26934130005405c5e5d9abec232ef98183032fce
Andrew Allison <andrewallison5211@gmail.com>
2024-01-22 12:53:11 -0700

    launch

--- client/src/models/AccountCampaignLink.js
+++
@@ -1,8 +0,0 @@
-export class AccountCampaignLink {
-  constructor(data) {
-    this.campaignId = data.campaignId
-    this.accountId = data.accountId
-    this.Campaign = data.Campaign
-    this.Profile = data.Profile
-  }
-}
